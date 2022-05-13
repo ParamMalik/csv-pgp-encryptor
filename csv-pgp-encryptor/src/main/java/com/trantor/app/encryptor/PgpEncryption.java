1ed3a960d4e17608cdb77a43ce1d7b79d0400bac
ParamMalik <86033103+ParamMalik@users.noreply.github.com>
2022-05-13 11:23:30 +0530

    encrypt-decrypt

--- csv-pgp-encryptor/src/main/java/com/trantor/app/encryptor/PgpEncryption.java
+++ csv-pgp-encryptor/src/main/java/com/trantor/app/encryptor/PgpEncryption.java
@@ -2,8 +2,14 @@ package com.trantor.app.encryptor;
 
 import org.springframework.stereotype.Component;
 
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.security.*;
 import java.util.Base64;
 
@@ -16,6 +22,8 @@ public class PgpEncryption {
     private final PublicKey publicKey;
 
     private static final String PADDING = "RSA/ECB/OAEPWithSHA-256AndMGF1Padding";
+
+    private static final String SAVE_ENCRYPTED_FILE_PATH = "D:\\Encrypted Files\\encrypted.pgp";
 
     public PgpEncryption() throws NoSuchAlgorithmException {
         KeyPairGenerator keyGenerator = KeyPairGenerator.getInstance("RSA");
@@ -42,8 +50,13 @@ public class PgpEncryption {
 
             byte[] encryptedBytes = cipher.doFinal(messageInBytes);
 
-            // encoding
+            // encoding using encode method
             String encodedMessage = encode(encryptedBytes);
+
+
+            // calling decrypt method
+            decrypt(encodedMessage);
+
 
             // writing file in the local folder
             writeEncryptedFileAsPgp(encodedMessage);
@@ -54,9 +67,36 @@ public class PgpEncryption {
     }
 
 
+        private byte[] decode(String data) {
+        return Base64.getDecoder().decode(data);
+    }
+
+
+
+
+    public void decrypt(String encryptedMessage) throws IllegalBlockSizeException, BadPaddingException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IOException {
+        byte[] encryptedBytes = decode(encryptedMessage);
+
+        Cipher cipher = Cipher.getInstance(PADDING);
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+        byte[] decryptedMessage = cipher.doFinal(encryptedBytes);
+
+        // Printing the decoded mssage
+        System.out.println("DECRYPTED MESSAGE : ");
+
+        ByteArrayInputStream bais = new ByteArrayInputStream(decryptedMessage);
+        DataInputStream in = new DataInputStream(bais);
+        while (in.available() > 0) {
+            String element = in.readUTF();
+            System.out.println(element);
+        }
+    }
+
+
+    // Writing encrypted file to  local folder
     public void writeEncryptedFileAsPgp(String encryptedMessage) {
 
-        try (FileWriter fileWriter = new FileWriter("D:\\Encrypted Files\\encrypted.pgp")) {
+        try (FileWriter fileWriter = new FileWriter(SAVE_ENCRYPTED_FILE_PATH)) {
             fileWriter.write(encryptedMessage);
 
         } catch (Exception exception) {
